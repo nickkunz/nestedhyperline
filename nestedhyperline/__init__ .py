0a0b01f49abad747a93f608e7ffb865867292b9a
Nick Kunz <nick.kunz@columbia.edu>
2020-04-05 12:38:18 -0700

    Add reg_param import

--- nestedhyperline/__init__ .py
+++ nestedhyperline/__init__ .py
@@ -6,6 +6,7 @@ from .results import *
 from .method_select import *
 from .ncv_optimizer import *
 from .argument_quality import *
+from .reg_params import *
 
 from .methods.ridge_ncv_regressor import *
 from .methods.lasso_ncv_regressor import *
