e7fec01015cc7a24afa9bb37c77c3dd01cd8c517
Nick Kunz <nick.kunz@columbia.edu>
2020-04-05 16:26:32 -0700

    Remove reg_param submodule import

--- nestedhyperline/regressors/__init__ .py
+++ nestedhyperline/regressors/__init__ .py
@@ -6,4 +6,3 @@ https://github.com/nickkunz/nestedhyperline
 from nestedhyperline.regressors.elastic_ncv_regressor import elastic_ncv_regressor
 from nestedhyperline.regressors.lasso_ncv_regressor import lasso_ncv_regressor
 from nestedhyperline.regressors.ridge_ncv_regressor import ridge_ncv_regressor
-from nestedhyperline.reg_params import reg_params, net_params
