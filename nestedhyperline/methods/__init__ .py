11e42ef1083ab54f5939a5ac3858f61965ed65b5
Nick Kunz <nick.kunz@columbia.edu>
2020-04-05 12:25:46 -0700

    Correct imports

--- nestedhyperline/methods/__init__ .py
+++ nestedhyperline/methods/__init__ .py
@@ -3,7 +3,7 @@ Nested Cross-Validation for Bayesian Hyper-Parameter Optimized Linear Regulariza
 https://github.com/nickkunz/nestedhyperline
 """
 
-from nestedhyperline.ridge_ncv_regressor import ridge_ncv_regressor
+from nestedhyperline.methods.ridge_ncv_regressor import ridge_ncv_regressor
 from nestedhyperline.methods.lasso_ncv_regressor import lasso_ncv_regressor
 from nestedhyperline.methods.elastic_ncv_regressor import elastic_ncv_regressor
 from nestedhyperline.reg_params import reg_params
