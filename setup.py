441c11fe33e98c019a826c577ac260fe2652aff1
Nick Kunz <nick.kunz@columbia.edu>
2020-04-05 14:29:18 -0700

    Update regressor submodule

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     ],
     packages = [
         'nestedhyperline',
-        'nestedhyperline.methods',
+        'nestedhyperline.regressors',
     ],
     include_package_data = True,
     install_requires = [
