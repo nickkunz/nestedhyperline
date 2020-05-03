ffd7f4c214658e1cfb2f26dd0a46acfba9f5effe
Nick Kunz <nick.kunz@columbia.edu>
2020-05-03 00:21:37 -0700

    Increase search space

--- nestedhyperline/reg_params.py
+++ nestedhyperline/reg_params.py
@@ -14,7 +14,7 @@ def reg_params():
         'alpha': hp.loguniform(
             label = 'alpha',
             low = np.log(0.00001),
-            high = np.log(100)
+            high = np.log(10000)
         )
     }
     
@@ -32,7 +32,7 @@ def net_params():
         'alpha': hp.loguniform(
             label = 'alpha',
             low = np.log(0.00001),
-            high = np.log(100)
+            high = np.log(10000)
         ),
         
         # l2 and l1 mixture
