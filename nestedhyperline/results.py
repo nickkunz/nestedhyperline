91e2e55bef4d0dbfd3da8f024edb443120b410c2
Nick Kunz <nick.kunz@columbia.edu>
2020-05-03 23:15:09 -0700

    Add lambda conv plot

--- nestedhyperline/results.py
+++ nestedhyperline/results.py
@@ -45,4 +45,107 @@ class RegressResults():
 
         return error_mean
     
-    ## 
+    ## plot convergence of lambda across outer k-folds
+    def plot_lambda(self,
+
+        ## pre-process args
+        trials_list = trials_list,
+        params = pararms,
+        k_outer = k_outer,
+        n_evals = n_evals,
+        standardize = standardize,
+
+        ## settings args
+        lw_dot = 1.30,
+        lw_sld = 1.1,
+        plt_hgt = 9,
+        plt_wdt = 20,
+        ax_fnt_sze = 11,
+        sty = 'whitegrid'
+        ):
+
+        ## plot pre-processor
+        plt_data = plot_prep(
+            trials_list = trials_list,
+            params = pararms,
+            k_outer = k_outer,
+            n_evals = n_evals,
+            standardize = standardize
+        )
+
+        ## style
+        sns.set_style(
+            style = sty
+        )
+
+        ## size
+        fig = plt.figure(
+            figsize = (plt_wdt, plt_hgt)
+        )
+
+        ## title
+        ax = fig.add_subplot(
+            title = "Nested K-Fold Cross-Validation: Bayesian Optimized Convergence of Log(λ)"
+        )
+
+        ## y axis
+        ax.set_ylabel(
+            ylabel = 'Log(λ)', 
+            fontsize = ax_fnt_sze
+        )
+
+        ax.set_ylim(
+            bottom = plt_data['lamb_min'] - (plt_data['lamb_min'] * 0.33), 
+            top = plt_data['lamb_max'] + (plt_data['lamb_max'] * 0.33)
+        )
+
+        ax.set_yscale(
+            value = 'log'
+        )
+
+        ## x axis
+        ax.set_xlabel(
+            xlabel = 'Number of Evaluations', 
+            fontsize = ax_fnt_sze
+        )
+
+        ax.set_xlim(
+            left = -(n_evals * 0.010), 
+            right = n_evals + (n_evals * 0.010)
+        )
+
+        ## bayesian optimization
+        for i in range(k_outer):
+            ax.scatter(
+                x = range(n_evals),
+                y = plt_data['lamb_list'][i],
+                color = plt_data['colors'][i], ## dot colors
+                alpha = 0.75,  ## dot opacity
+                s = 12  ## dot size
+            )
+
+        ## lowest average test error line
+        ax.axhline(
+            y = plt_data['lamb_mean_test'],
+            color = 'black',
+            linestyle = 'dotted',
+            linewidth = lw_dot
+        )
+
+        ## lowest average train-valid error line
+        ax.axhline(
+            y = plt_data['lamb_mean_min'],
+            color = 'black',
+            linewidth = lw_sld
+        )
+
+        ## legend
+        ax.legend(
+            handles = plt_data['leg_lines'], 
+            labels = plt_data['leg_labels'],
+            loc = 'lower right',
+            fontsize = 'small'
+        )
+
+        ## display
+        plt.show()
