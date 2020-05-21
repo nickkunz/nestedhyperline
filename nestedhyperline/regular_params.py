557f4534b59e21a2c4e4a9df77e983b0f154315b
Nick Kunz <nick.kunz@columbia.edu>
2020-05-21 16:55:20 -0700

    Remove whitespace

--- nestedhyperline/regular_params.py
+++ nestedhyperline/regular_params.py
@@ -4,10 +4,10 @@ from hyperopt import hp
 
 ## ridge and lasso hyper-params
 def reg_params():
-    
+
     """ Utilized for Bayesian Optimization. Returns Ridge and 
     LASSO regression parameter ranges (search space). """
-    
+
     reg_params = {
         
         ## lambda shrinkage
@@ -17,24 +17,24 @@ def reg_params():
             high = np.log(10000)
         )
     }
-    
+
     return reg_params
 
 ## elastic-net hyper-params
 def net_params():
-    
+
     """ Utilized for Bayesian Optimization. Returns Elastic-Net 
     regression parameter ranges (search space) """
-    
+
     net_params = {
-        
+
         ## lambda shrinkage
         'alpha': hp.loguniform(
             label = 'alpha',
             low = np.log(0.00001),
             high = np.log(10000)
         ),
-        
+
         # l2 and l1 mixture
         'l1_ratio': hp.uniform(
             label = 'l1_ratio',
